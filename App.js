c2003f028b7f635551606321d7e044c2df701730
xiaoju <me@xiaoju.io>
2017-11-14 11:57:12 +0100

    feat: history page

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
 
@@ -11,7 +12,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     )
