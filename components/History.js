a4517cfea95c686c6fde5927626943ae917e33a7
xiaoju <me@xiaoju.io>
2017-11-14 12:51:21 +0100

    feat: app loading screen

--- components/History.js
+++ components/History.js
@@ -8,8 +8,13 @@ import UdaciFitnessCalendar from 'udacifitness-calendar'
 import { white } from '../utils/colors'
 import DateHeader from './DateHeader'
 import MetricCard from './MetricCard'
+import { AppLoading} from 'expo'
 
 class History extends Component {
+  state = {
+    ready: false,
+  }
+
   componentDidMount () {
     const { dispatch } = this.props
 
@@ -24,6 +29,7 @@ class History extends Component {
       })
       .then(() => this.setState(() => ({ready: true})))
   }
+
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
     <View style={styles.item}>
       {today
@@ -52,6 +58,11 @@ class History extends Component {
   }
   render() {
     const { entries } = this.props
+    const { ready } = this.state
+
+    if (ready === false) {
+      return <AppLoading />
+    }
 
     return (
       <UdaciFitnessCalendar
